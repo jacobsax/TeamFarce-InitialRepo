139b8911df8d70f27496ff00887ac2d01e1cabe8
Jacob Unwin <jacobsax@gmail.com>
2017-01-16 21:51:55 -0800

    Fixed missing classes/functions
    Created new QuestionandResponse class, added missing functions to
    Journal class. Updated .gitignore file to include .bash_profile

--- game/src/main/java/org/teamfarce/mirch/Journal.java
+++ game/src/main/java/org/teamfarce/mirch/Journal.java
@@ -2,4 +2,14 @@ package org.teamfarce.mirch;
 
 public class Journal {
 
+	public void addProp(Prop prop) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void addDialogue(QuestionandResponse dialogue) {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
