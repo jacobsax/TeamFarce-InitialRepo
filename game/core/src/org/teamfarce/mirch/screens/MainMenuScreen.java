ca5c0f7ea44a030a1e44fa16b6adf375afc1fd99
Jacob Unwin <jacobsax@gmail.com>
2017-04-27 16:54:31 +0100

    changed menu branding

--- game/core/src/org/teamfarce/mirch/screens/MainMenuScreen.java
+++ game/core/src/org/teamfarce/mirch/screens/MainMenuScreen.java
@@ -69,7 +69,7 @@ public class MainMenuScreen extends AbstractScreen {
         LabelStyle textStyle = new LabelStyle(font, Color.WHITE);
 
         // Creating the label containing text and determining its size and location on screen
-        Label text = new Label("Welcome to the Lorem Ipsum Murder Mystery Game!", textStyle);
+        Label text = new Label("Welcome to the Team FARCE Murder Mystery Game!", textStyle);
 
         text.setFontScale(2, 2);
         text.setBounds(
